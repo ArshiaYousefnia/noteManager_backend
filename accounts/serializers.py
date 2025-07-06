6f8c12cea9f875cd44ee3589f1c341d8a63e5435
arshia-yousefnia <arshia.yousefnia@gmail.com>
2025-07-06 11:09:18 +0330

    project initaited

---
+++ accounts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'bio',)
+        read_only_fields = ('username')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
