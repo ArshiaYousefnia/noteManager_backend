70dc69241a1f8969ddcafc2a8d31c6f7a16f80db
ArshiaYousefia <arshia.yousefnia@gmail.com>
2025-07-12 18:01:26 +0330

    notes: title added to note

--- notes/serializers.py
+++ notes/serializers.py
@@ -6,7 +6,7 @@ from notes.models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ('uuid', 'content', 'created_at', 'updated_at')
+        fields = ('uuid','title' ,'content', 'created_at', 'updated_at')
         read_only_fields = ('uuid', 'created_at', 'updated_at')
 
     def __init__(self, *args, **kwargs):
